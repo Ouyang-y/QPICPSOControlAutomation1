63de754869b96f012d3c75248022686ed7b6bd13
ouyang <5147425@qq.com>
2026-05-12 16:02:05 +0800

    V1.0.0  #ALLIN1-101 实现核心功能 + 定义controller变量，并链接设备进行初始化(FormMain.cs/Line 10) + 按钮逻辑(FormMain.cs/Line 50--66) + 控制箱报错((FormMain.cs/Line 14、25、67--77))

---
+++ FormMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace QPICPSOControlAutomation1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button Button1;
+            Button1 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // Button1
+            // 
+            Button1.BackColor = System.Drawing.SystemColors.Control;
+            Button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            Button1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            Button1.Location = new System.Drawing.Point(92, 58);
+            Button1.Name = "Button1";
+            Button1.Size = new System.Drawing.Size(173, 122);
+            Button1.TabIndex = 0;
+            Button1.Text = "Laser Off";
+            Button1.UseVisualStyleBackColor = false;
+            Button1.Click += new System.EventHandler(Button1_Click);
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(361, 238);
+            Controls.Add(Button1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Name = "FormMain";
+            Text = "PSOControl";
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,79 @@
+﻿using Aerotech.Automation1.DotNet;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace QPICPSOControlAutomation1
+{
+    public partial class FormMain : Form
+    {
+        private Controller controller = Controller.Connect();  // 定义controller变量，并链接设备进行初始化
+        public FormMain()
+        {
+            InitializeComponent();  // 初始化窗体
+            this.FormClosing += FormMain_FormClosing;  // 链接窗体关闭句柄
+        }
+
+        #region 窗口打开/关闭
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // 打开时链接控制箱报错
+                // We can also subscribe to the ExceptionOccurred event to be notified of any errors that occur
+                // while the controller is retrieving status. If this event is raised, it will mean the status retrieval has been paused.
+                controller.Runtime.Status.Automatic.ExceptionOccurred += controller_ExceptionOccurred;
+
+                // 打开时设定PsoOutputPin，依据硬件连接(当前为XR3，Output2，链接引脚10&12)
+                // https://help.aerotech.com/automation1/hardware-manuals/Automation1-iXR3-and-XR3-web/Chapter-2-Installation-and-Configuration/Position-Synchronized-Output-XR3.htm
+                controller.Runtime.Commands.Pso.PsoOutputConfigureOutput("X", PsoOutputPin.XR3PsoOutput2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"控制器连接/启动失败: {ex.Message}", "错误");
+            }
+        }
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            controller.Runtime.Status.Automatic.ExceptionOccurred -= controller_ExceptionOccurred;
+            try
+            {
+                controller?.Disconnect(); // 断连接
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"关闭时出错: {ex.Message}");
+            }
+        }
+        #endregion
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.Text == "Laser On")
+            {
+                controller.Runtime.Commands.Pso.PsoOutputOff("X");
+                btn.Text = "Laser Off";
+                btn.BackColor = SystemColors.Control;  // 恢复默认
+            }
+            else
+            {
+                controller.Runtime.Commands.Pso.PsoOutputOn("X");
+                btn.Text = "Laser On";
+                btn.BackColor = Color.PaleGreen;
+            }            
+        }
+        private void controller_ExceptionOccurred(object sender, ExceptionOccurredEventArgs e)
+        {
+            // Since we want to use the new status from this event to update elements in the UI, we
+            // must use Invoke() to have the code that processes the new status run on the main UI thread.
+            // See the following Microsoft documentation for details on making thread-safe calls to WinForms controls.
+            // https://docs.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-make-thread-safe-calls-to-windows-forms-controls
+            Invoke(new Action(() =>
+            {
+                MessageBox.Show($"{e.Exception.Message}", "Controller Error");
+            }));
+        }
+    }
+}
