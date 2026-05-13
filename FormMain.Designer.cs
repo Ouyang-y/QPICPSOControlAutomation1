97ba8fb202e259ffee26411fdf0c1704bbf4f4fe
ouyang <5147425@qq.com>
2026-05-13 16:50:00 +0800

    V1.1.0  #ALLIN1-101 功能完善 + 添加窗口位置记忆 (Setting.settings、FormMain.cs/Line 61--62、74--79、FormMain.Designer.cs/Line 56) + 添加窗口置顶，以及窗口失焦透明40% (FormMain.cs/Line 90--108) + 添加窗口移动限制，不允许移出主屏幕 (FormMain.cs/Line 109--129) + 修复连接设备未运行控制箱报错 (FormMain.cs/Line 15、35--36) + 修复防闪烁逻辑 (FormMain.cs/Line 63、FormMain.Designer.cs/Line 58) + 添加图标

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -37,24 +37,31 @@
             Button1.Cursor = System.Windows.Forms.Cursors.Hand;
             Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             Button1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            Button1.Location = new System.Drawing.Point(92, 58);
+            Button1.Location = new System.Drawing.Point(12, 12);
             Button1.Name = "Button1";
-            Button1.Size = new System.Drawing.Size(173, 122);
+            Button1.Size = new System.Drawing.Size(271, 149);
             Button1.TabIndex = 0;
             Button1.Text = "Laser Off";
             Button1.UseVisualStyleBackColor = false;
-            Button1.Click += new System.EventHandler(Button1_Click);
+            Button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 238);
+            this.ClientSize = new System.Drawing.Size(295, 173);
             this.Controls.Add(Button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "PSOControl";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Activated += new System.EventHandler(this.FormMain_Activated);
+            this.Deactivate += new System.EventHandler(this.FormMain_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
+            this.Move += new System.EventHandler(this.FormMain_Move);
             this.ResumeLayout(false);
 
         }
